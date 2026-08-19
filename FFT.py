9461f6949d58c44c7f23130c3a378039f052aec9
morsolad <morsolad@gmail.com>
2026-08-19 13:11:06 +0100

    Created a working FFT

--- FFT.py
+++ FFT.py
@@ -1,1 +1,31 @@
+from cmath import exp, pi
 
+def FFT(f): # f is an array
+    N = len(f)
+    if N & (N-1) != 0:
+        Temp=N.bit_length() - 1
+        Offset=N-2**Temp
+        if Offset>0:
+            Temp+=1
+            Offset=abs(N-2**Temp)    
+            f = f + [0]*Offset
+        N=len(f)
+    return FFT_recursion(f)
+
+
+def FFT_recursion(f):
+
+    N = len(f)
+
+    if N == 1:
+        return [f[0]]
+
+    farray_even = FFT_recursion(f[::2]) # size N/2
+    farray_odd = FFT_recursion(f[1::2] ) # size N/2
+
+    farray = [0] * N
+    for p in range(0, (N//2)):
+
+        farray[p] = farray_even[p] +exp(1j*2*pi*p/N)*farray_odd[p]
+        farray[p+N//2] = farray_even[p] - exp (1j*2*pi*p/N)* farray_odd[p]
+    return farray
